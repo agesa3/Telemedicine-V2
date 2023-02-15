821271be94a4e1d360ef7523e4f9ebc8f6797ecf
agesa3 <collinsagesa2000@gmail.com>
2023-02-15 22:51:48 +0300

    dialog fragment ui added

--- app/src/main/java/com/agesadev/telmedv2/presentation/forms/AddDialogFragment.kt
+++ app/src/main/java/com/agesadev/telmedv2/presentation/forms/AddDialogFragment.kt
@@ -5,19 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.agesadev.telmedv2.R
 
-class MedicalHistoryFragment : Fragment() {
+
+class AddDialogFragment : DialogFragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medical_history, container, false)
+        return inflater.inflate(R.layout.fragment_add_dialog, container, false)
     }
 }
