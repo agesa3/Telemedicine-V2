6cb2afc40107f7e195a831909014b90018601c88
agesa3 <collinsagesa2000@gmail.com>
2023-02-22 16:28:42 +0300

    added patient recycler adapater

--- app/src/main/java/com/agesadev/telmedv2/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/agesadev/telmedv2/presentation/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.agesadev.telmedv2.presentation.home
 
+import android.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -18,25 +20,38 @@ class HomeFragment : Fragment() {
     private var _homeBinding: FragmentHomeBinding? = null
     private val homeBinding get() = _homeBinding
     private val authViewModel: AuthViewModel by viewModels()
+    private val patientsRecyclerAdapter: PatientsRecyclerAdapter = PatientsRecyclerAdapter()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        homeBinding?.logoutImageIcon?.setOnClickListener {
+            authViewModel.logout()
+            findNavController().navigate(R.id.loginFragment)
+        }
         return homeBinding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        homeBinding?.toolbar?.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.logout_icon -> {
-                    authViewModel.logout()
-                    findNavController().navigate(R.id.loginFragment)
-                }
-            }
-            true
-        }
+        setUpRecyclerView()
+//        homeBinding?.toolbar?.setOnMenuItemClickListener {
+//            when (it.itemId) {
+//                R.id.logout_icon -> {
+//                    Toast.makeText(requireContext(), "Logout", Toast.LENGTH_SHORT).show()
+////                    authViewModel.logout()
+////                    findNavController().navigate(R.id.loginFragment)
+//                }
+//            }
+//            true
+//        }
     }
+
+    private fun setUpRecyclerView() {
+        homeBinding?.patientsRecyclerView?.adapter = patientsRecyclerAdapter
+    }
+
 }
--- app/src/main/java/com/agesadev/telmedv2/presentation/home/PatientsRecyclerAdapter.kt
+++ app/src/main/java/com/agesadev/telmedv2/presentation/home/PatientsRecyclerAdapter.kt
@@ -1,4 +1,49 @@
 package com.agesadev.telmedv2.presentation.home
 
-class PatientsRecyclerAdapter {
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.agesadev.telmedv2.data.models.PersonalInfo
+import com.agesadev.telmedv2.databinding.SinglePatientCardBinding
+
+class PatientsRecyclerAdapter :
+    ListAdapter<PersonalInfo, PatientsRecyclerAdapter.PatientViewHolder>(patientsDiffUtil) {
+
+    inner class PatientViewHolder(val binding: SinglePatientCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(patient: PersonalInfo) {
+            binding.apply {
+                patientName.text = patient.fullName
+                patientDescription.text = patient.idNo.toString()
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatientViewHolder {
+        return PatientViewHolder(
+            SinglePatientCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: PatientViewHolder, position: Int) {
+        val patient = getItem(position)
+        holder.bind(patient)
+    }
+
+}
+
+val patientsDiffUtil = object : DiffUtil.ItemCallback<PersonalInfo>() {
+    override fun areItemsTheSame(oldItem: PersonalInfo, newItem: PersonalInfo): Boolean {
+        return oldItem.idNo == newItem.idNo
+    }
+
+    override fun areContentsTheSame(oldItem: PersonalInfo, newItem: PersonalInfo): Boolean {
+        return oldItem == newItem
+    }
+
 }
